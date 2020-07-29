65f1a188325a8cfd6b3076d76b69167365b236c8
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-28 22:59:02 -0500

    commit

---
+++ pyrobot/robot.py
@@ -0,0 +1,35 @@
+import json
+import time as time_true
+import pprint
+import pathlib
+import pandas as pd
+
+from typing import List
+from typing import Dict
+from typing import Union
+from typing import Optional
+
+from td.client import TDClient
+from td.utils import milliseconds_since_epoch
+
+from datetime import time
+from datetime import time
+from datetime import timezone
+
+
+class PyRobot():
+
+
+    def __init__(self, client_id: str, redirect_uri str, credentials_path: str = None, trading account: str = None) -> None:
+        self.client_id = client_id
+
+
+
+
+
+
+
+
+
+
+
