e204338ee6b58d8e017990fd90908c99186c2c1a
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-30 06:22:34 -0500

    commit

--- stock.py
+++ stock.py
@@ -1,13 +1,31 @@
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.timeseries import as av
 
 
 # input your key here
-key = '7QZ7M7T8FHWLUAX1'
+key = 'XXX'
 
 ts = TimeSeries(key)
 
 aapl, meta = ts.get_daily(symbol="AAPL")
 print(aapl["2020-07-29"])
+
+class TimeSeries(av):
+    """this class implements all the api calls to time series
+    """
+    def get_intraday(self, symbol, interval='15min', outputsize='compact', __function_key='XXX'):
+        """Return intraday time series in two json objects as data and meta_data.
+        It raises ValueError when problems arise
+
+        Keyword Arguments:
+            symbol:TSLA
+            interval: 30min
+            outputsize:full
+
+            """
+        __function_key = "TIME_SERIES_INTRADAY"
+        return __function_key, "Time Series ({})".format(interval), 'Meta Data'
+
 
 
 
--- techindicators.py
+++ techindicators.py
@@ -1,5 +1,8 @@
 from alpha_vantage.techindicators import av
 
+# input key
+key = 'XXX'
+
 
 class TechIndicators(av):
     def __init__(self, *args, **kwargs):
@@ -9,7 +12,7 @@ class TechIndicators(av):
             raise ValueError("Output Format {} is not compatible with the TechIndicators class".format(
                 self.output_format.lower()))
 
-    def get_sma(self, symbol, interval='daily', time_period=20, series_type='close'):
+    def get_sma(self, symbol, interval='daily', time_period=20, series_type='close', __function_key='XXX'):
         """ return simple moving average time series in two json objects as data and meta_data. It raises ValueError
         when problems arise
 
@@ -20,4 +23,4 @@ class TechIndicators(av):
             series_type: high
             """
         __FUNCTION_KEY__ = "SMA"
-        return __FUNCTION_KEY__, 'Technical Analysis: SMA', 'Meta Data'
+        return __function_key, 'Technical Analysis: SMA', 'Meta Data'
