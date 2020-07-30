5ee30aaedcd486fec9a16e0af5e437ba6d340d66
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-30 06:07:35 -0500

    commit

--- stock.py
+++ stock.py
@@ -9,6 +9,7 @@ ts = TimeSeries(key)
 aapl, meta = ts.get_daily(symbol="AAPL")
 print(aapl["2020-07-29"])
 
+
 class TechIndicators(av):
     def __init__(self, *args, **kwargs):
         super(TechIndicators, self).__init__(*args, **kwargs)
@@ -17,25 +18,16 @@ class TechIndicators(av):
             raise ValueError("Output Format {} is not compatible with the TechIndicators class".format(
                 self.output_format.lower()))
 
-    def get_sma(self):
+    def get_sma(self, symbol, interval='daily', time_period=20, series_type='close'):
+        """ return simple moving average time series in two json objects as data and meta_data. It raises ValueError
+        when problems arise
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Keyword Arguments:
+            symbol: AAPL
+            interval: 15min
+            time_period: 20
+            series_type: high
+            """
+        __FUNCTION_KEY__ = "SMA"
+        return __FUNCTION_KEY__, 'Technical Analysis: SMA', 'Meta Data'
 
