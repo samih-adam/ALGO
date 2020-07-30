0da2418e566338b3a8af75e290b526621109ce27
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-30 05:53:52 -0500

    commit

--- stock.py
+++ stock.py
@@ -1,10 +1,32 @@
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import av
+
 # input your key here
 key = 'XXX'
 
 ts = TimeSeries(key)
 
 aapl, meta = ts.get_daily(symbol="AAPL")
+print(aapl["2020-07-29"])
+
+class TechIndicators(av):
+    def __init__(self, *args, **kwargs):
+        super(TechIndicators, self).__init__(*args, **kwargs)
+        self._append_type = False
+        if self.output_format.lower() == 'csv':
+            raise ValueError("Output Format {} is not compatible with the TechIndicators class".format(
+                self.output_format.lower()))
+
+    def get_sma(self):
+
+
+
+
+
+
+
+
+
 
 
 
