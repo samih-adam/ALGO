78420005489a6c13f05d6e6217af0d6f88ef64cf
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-30 05:22:42 -0500

    commit

---
+++ stock.py
@@ -0,0 +1,23 @@
+import aiohttp
+from functools import wraps
+import inspect
+import re
+from .alphavantage import AlphaVantage as av
+
+
+function=CURRENCY_EXCHANGE_RATE
+from_currency=USD
+to_currency=BTC
+
+apikey = 7QZ7M7T8FHWLUAX1
+
+function =CRYPTO_RATING
+
+
+
+
+
+
+
+
+
